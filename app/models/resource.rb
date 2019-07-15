36f5ac326163b702c101da5b7a598a99f56734b3
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-07-14 22:07:45 -0600

    Adds belongs to user and belongs to resource_type to resource model

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,6 +1,8 @@
 class Resource < ApplicationRecord
   validates_presence_of :name
 
+  belongs_to :resource_type
+  belongs_to :user
   has_many :resource_parts
   has_many :parts, through: :resource_parts
 
