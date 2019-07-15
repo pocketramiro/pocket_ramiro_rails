582418cc41887bfe9673a3dbc003e4aba6086d39
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-07-14 22:13:55 -0600

    Adds update resource spec

---
+++ spec/requests/api/v1/resources/update_spec.rb
@@ -0,0 +1,20 @@
+require "rails_helper"
+
+RSpec.describe "as a registered user", type: :request do
+  it "I can see all the active resources in the system" do
+    user  = User.create(id: 1, name: "Cameron Marks", email: "cameron_marks@greatdivide.com", password_digest: "password", role: 0, phone_number: "7208674848", active: true, created_at: "2015-11-29 00:00:00", updated_at: "2019-06-01 00:00:00")
+    r_type1 = ResourceType.create(category: "Bright Tank", company: "Mueller", contact_number: 5419307880, contact_name: "Sheryl Michiel", active: true,manual_url: "https://en.wikipedia.org/wiki/Donald_Cameron_(mayor", created_at: "1994-06-22 00:00:00", updated_at: "2000-01-01 00:00:00",id: 1)
+    resource1 = r_type1.resources.create(name: "Bright Tank 1", cost:20000.00, user_id: 1, created_at: "2008-02-09 16:49:29", updated_at: "2008-02-09 16:49:29", active: true,id: 1)
+
+    fixed_resource = {name: "Bright Tank 4"}
+
+    patch "/api/v1/resources", params: fixed_resource
+    expect(response).to be_successful
+    results = JSON.parse(response.body, symbolize_names: true)
+
+    expect(results[:name]).to eq("Bright Tank 4")
+    expect(results[:cost]).to eq(20000.00)
+    expect(results[:user_id]).to eq(1)
+    expect(results[:active]).to eq(true)
+  end
+end
