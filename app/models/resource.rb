96e95d2754e7e366edd1c49b4ad1dca2a26f9f7b
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-07-14 20:49:17 -0600

    Adds all_active_resources class method

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,7 +1,10 @@
 class Resource < ApplicationRecord
   validates_presence_of :name
-  
+
   has_many :resource_parts
   has_many :parts, through: :resource_parts
 
+  def self.all_active_resources
+    where(active:true)
+  end
 end
