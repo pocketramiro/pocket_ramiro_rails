b54a952536bbc3e248d9483de7fe0dbbe8c4f825
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-07-15 09:15:50 -0600

    Adds destroy_all to Tables in resources and notes tests

--- spec/requests/api/v1/resources/notes/create_spec.rb
+++ spec/requests/api/v1/resources/notes/create_spec.rb
@@ -1,40 +1,39 @@
-require "rails_helper"
-
 RSpec.describe "as a registered user", type: :request do
-  describe "When I go to the machine dashboard" do
+  describe "when I visit a resource's page" do
     before :each do
+      Note.destroy_all
       Resource.destroy_all
-      User.destroy_all
       ResourceType.destroy_all
+      User.destroy_all
       @user  = User.create(id: 1, name: "Cameron Marks", email: "cameron_marks@greatdivide.com", password_digest: "password", role: 0, phone_number: "7208674848", active: true, created_at: "2015-11-29 00:00:00", updated_at: "2019-06-01 00:00:00")
       @r_type1 = ResourceType.create(category: "Bright Tank", company: "Mueller", contact_number: 5419307880, contact_name: "Sheryl Michiel", active: true,manual_url: "https://en.wikipedia.org/wiki/Donald_Cameron_(mayor", created_at: "1994-06-22 00:00:00", updated_at: "2000-01-01 00:00:00",id: 1)
       @resource1 = @r_type1.resources.create(name: "Bright Tank 1", cost:20000.00, user_id: 1, created_at: "2008-02-09 16:49:29", updated_at: "2008-02-09 16:49:29", active: true,id: 1)
     end
 
-    it "I can update an existing resource" do
-      fixed_resource = {name: "Bright Tank 4"}
+    it "I can create a new note for that resource" do
+      new_note = {id: 3,
+                   user_id: 1,
+                   table_key: 1,
+                   table_name: "Resources",
+                   content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                   created_at: "2019-07-05 20:35:50",
+                   updated_at: "2019-07-10 20:35:50"}
 
-      patch "/api/v1/resources/1", params: fixed_resource
+
+      post "/api/v1/resources/1/notes", params: new_note
+
       expect(response).to be_successful
       results = JSON.parse(response.body, symbolize_names: true)
+      note = Note.last
 
-      expect(results[:name]).to eq("Bright Tank 4")
-      expect(results[:cost]).to eq(20000.00)
-      expect(results[:user_id]).to eq(1)
-      expect(results[:active]).to eq(true)
+      expect(results.count).to eq(3)
+      expect(results[0][:id]).to eq(1)
+      expect(results[0][:user_id]).to eq(1)
+      expect(results[0][:table_key]).to eq(1)
+      expect(results[0][:table_name]).to eq("Resources")
     end
 
-    # patch "cannot find Resource with that id" - yes I know
-    xit "I cannot update an existing resource if it does not exist" do
-      fixed_resource = {name: "Bright Tank 4"}
-
-      patch "/api/v1/resources/2", params: fixed_resource
-      # expect(response).to be_successful
-      results = JSON.parse(response.body, symbolize_names: true)
-
-      expect(results).to eq({
-        "Error": "Resource could not be created."
-        })
+    it "does not allow me to create a note without a user or resource" do
 
     end
   end
