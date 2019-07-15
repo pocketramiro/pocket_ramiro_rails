26c12e09ed15db4f8da528b5971af2bf63091503
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-07-15 14:00:12 -0600

    Adds show, create, and update for User

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,32 @@
+class Api::V1::UsersController < ApplicationController
+
+  def show
+    user = User.find(params[:id])
+    render json: user
+  end
+
+  def create
+    user = User.create(user_params)
+    if user.save
+      render json: user
+    else
+      render json: {
+        "Error": "User could not be created."
+      }
+    end
+  end
+
+  def update
+    user = User.find(params[:id])
+    if user
+      user.update(user_params)
+      updated_user = User.find(params[:id])
+      render json: updated_user
+    else
+      render json: {
+        "Error": "User does not exist in database."
+      }
+    end
+  end
+
+end
