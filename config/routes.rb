513ce299144a3e7b06b120f2e125964a9a69b112
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-07-14 20:38:08 -0600

    Adds resources index route

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,20 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      resources :resources, only: [:index]
+
       get '/tickets', to: 'tickets#index'
-    end
-  end
-  namespace :api do
-    namespace :v1 do
+
       namespace :resources do
         get ':id/tickets', to: 'tickets#index'
         get ':id/tickets/:ticket_id', to: 'tickets#show'
         post ':id/tickets', to: 'tickets#create'
       end
-    end
-  end
-  namespace :api do
-    namespace :v1 do
+
       namespace :parts do
         get ':id/tickets', to: 'tickets#index'
       end
+
     end
   end
 end
