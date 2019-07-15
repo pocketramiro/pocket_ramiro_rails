584dbf5d9063854cfe6f312a16fca9df3b857057
JennicaStiehl <jennica.stiehl@gmail.com>
2019-07-14 18:05:28 -0600

    Adds create action for tickets on a specific resource

--- app/controllers/api/v1/resources/tickets_controller.rb
+++ app/controllers/api/v1/resources/tickets_controller.rb
@@ -4,4 +4,25 @@ class Api::V1::Resources::TicketsController < ApplicationController
     render json: results
   end
 
+  def create
+    ticket = Ticket.new(
+      table_name: params["table_name"],
+      table_key: params["id"].to_i,
+      notes: params["notes"],
+      user_id: params["user_id"],
+      priority: params["priority"].to_i
+    )
+    if ticket.save
+      render json: {status: "201",
+      body: {
+        "message": "You have successfully created a ticket."
+        }}
+    else
+        render json: {status: "406",
+        body: {
+          "message": "Unable to create your ticket."
+          }}
+    end
+  end
+
 end
