5b7fe738c74063a61a156f715fb0d42491fa7a34
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-07-15 14:05:47 -0600

    Adds destroy_all to before :each

--- spec/requests/api/v1/resource_types/index_spec.rb
+++ spec/requests/api/v1/resource_types/index_spec.rb
@@ -2,6 +2,10 @@ require "rails_helper"
 
 RSpec.describe "as a registered user", type: :request do
   it "I can see all the active resource_types in the system" do
+    Resource.destroy_all
+    User.destroy_all
+    ResourceType.destroy_all
+    
     user  = User.create(id: 1, name: "Cameron Marks", email: "cameron_marks@greatdivide.com", password_digest: "password", role: 0, phone_number: "7208674848", active: true, created_at: "2015-11-29 00:00:00", updated_at: "2019-06-01 00:00:00")
     r_type1 = ResourceType.create(category: "Bright Tank", company: "Mueller", contact_number: 5419307880, contact_name: "Sheryl Michiel", active: true,manual_url: "https://en.wikipedia.org/wiki/Donald_Cameron_(mayor", created_at: "1994-06-22 00:00:00", updated_at: "2000-01-01 00:00:00",id: 1)
     r_type1 = ResourceType.create(category: "Bottler", company: "Mueller", contact_number: 5419307880, contact_name: "Sam Michiel", active: true,manual_url: "https://en.wikipedia.org/wiki/Donald_Cameron_(mayor", created_at: "1994-06-22 00:00:00", updated_at: "2000-01-01 00:00:00",id: 2)
--- spec/requests/api/v1/users/show_spec.rb
+++ spec/requests/api/v1/users/show_spec.rb
@@ -3,23 +3,22 @@ require "rails_helper"
 RSpec.describe "as a registered user", type: :request do
 
 
-  it "I can see a specific resource type in the system" do
+  it "I can see a specific resource in the system" do
     Resource.destroy_all
     User.destroy_all
     ResourceType.destroy_all
-    
     user  = User.create(id: 1, name: "Cameron Marks", email: "cameron_marks@greatdivide.com", password_digest: "password", role: 0, phone_number: "7208674848", active: true, created_at: "2015-11-29 00:00:00", updated_at: "2019-06-01 00:00:00")
-    r_type1 = ResourceType.create(category: "Bright Tank", company: "Mueller", contact_number: 5419307880, contact_name: "Sheryl Michiel", active: true,manual_url: "https://en.wikipedia.org/wiki/Donald_Cameron_(mayor", created_at: "1994-06-22 00:00:00", updated_at: "2000-01-01 00:00:00",id: 1)
 
-    get "/api/v1/resource_types/1"
+    get "/api/v1/users/1"
 
     expect(response).to be_successful
     results = JSON.parse(response.body, symbolize_names: true)
 
-    expect(results[:category]).to eq("Bright Tank")
-    expect(results[:company]).to eq("Mueller")
-    expect(results[:contact_name]).to eq("Sheryl Michiel")
+    expect(results[:name]).to eq("Cameron Marks")
+    expect(results[:email]).to eq("cameron_marks@greatdivide.com")
+    expect(results[:user_id]).to eq(1)
     expect(results[:active]).to eq(true)
+    expect(results[:phone_number]).to eq("7208674848")
   end
 
   # need to add test for if id is not active
