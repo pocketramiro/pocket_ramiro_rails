5b60389b07820b8c5c85e181dc760a3799be9f3e
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-07-20 11:42:28 -0600

    Adds routes for sessions for create,destroy
    Co-authored-by: Jennica Stiehl <25069483+stiehlrod@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -29,6 +29,8 @@ Rails.application.routes.draw do
       resources :resource_types, only: [:index, :create, :show, :update]
 
       resources :users, only: [:show, :create, :update]
+
+      resources :sessions, only: [:create, :destroy]
     end
   end
 end
