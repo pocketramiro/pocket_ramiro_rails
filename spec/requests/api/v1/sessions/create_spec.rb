513ad76d2853ba95a531b7b1d20de2a3fdb2fff8
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-07-20 12:03:11 -0600

    Finishes create session spec
    Co-authored-by: Jennica Stiehl <25069483+stiehlrod@users.noreply.github.com>

--- spec/requests/api/v1/sessions/create_spec.rb
+++ spec/requests/api/v1/sessions/create_spec.rb
@@ -6,18 +6,19 @@ RSpec.describe "As a registered user", type: :request do
     Resource.destroy_all
     ResourceType.destroy_all
     User.destroy_all
-    @user = User.create(id: 1, name: "Cameron Marks", email: "cameron_marks@greatdivide.com", password_digest: "password", role: "admin", phone_number: 7208674848, active: true, created_at: "2015-11-29 00:00:00", updated_at: "2019-06-01 00:00:00")
+    @user = User.create(id: 1, name: "Cameron Marks", email: "cameron_marks@greatdivide.com", password: "password", role: "admin", phone_number: 7208674848, active: true, created_at: "2015-11-29 00:00:00", updated_at: "2019-06-01 00:00:00")
   end
 
   describe "when I enter the correct credentials" do
     it "creates a session" do
-      binding.pry
 
       post("/api/v1/sessions?email=#{@user.email}&password=#{@user.password}")
 
       expect(response.status).to eq(200)
       body = JSON.parse(response.body, symbolize_names: true)
-      # expect(body[:api_key]).to eq("asdf")
+      expect(body[:session][:user_id]).to eq(@user.id)
+      expect(body[:session][:name]).to eq(@user.name)
+      expect(body[:session][:role]).to eq(@user.role)
     end
   end
 
