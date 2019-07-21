91aa70b8b5c0f9ffca4f5823f8daa72a05c1394d
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-07-21 17:44:23 -0600

    Adds user serializer spec and includes user in ticket serializer

--- spec/serializers/user_serializer_spec.rb
+++ spec/serializers/user_serializer_spec.rb
@@ -9,17 +9,13 @@ RSpec.describe TicketSerializer do
       ResourceType.destroy_all
 
       user  = User.create(id: 1, name: "Cameron Marks", email: "cameron_marks@greatdivide.com", password: "password", role: 0, phone_number: 7208674848, active: true, created_at: "2015-11-29 00:00:00", updated_at: "2019-06-01 00:00:00")
-      ResourceType.create(id: 1, category:"vehicle", company:"Brew Bears")
-      Resource.create(id: 1, resource_type_id:1, name:"van", cost:"75000", user_id:1)
-      ticket = Ticket.create(table_key: 1, table_name: "Resources",user_id:1, notes:"needs oil change", priority:1)
 
-      serializer = TicketSerializer.new(ticket, include: [:user])
+      serializer = UserSerializer.new(user)
 
-
+      # binding.pry
       response = JSON.parse(serializer.to_json, symbolize_names: true)
-
-      expect(response[:included][0][:attributes][:name]).to eq("Cameron Marks")
-      expect(response[:included][0][:attributes][:id]).to eq(user.id)
+      expect(response[:data][:attributes][:name]).to eq("Cameron Marks")
+      expect(response[:data][:attributes][:id]).to eq(user.id)
     end
   end
 end
