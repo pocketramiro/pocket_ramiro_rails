91ecdac6e62df945dacb39dc06af14d940f12990
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-07-22 15:59:26 -0600

    Changes render from create user to message

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -14,7 +14,7 @@ class Api::V1::UsersController < ApplicationController
     if user_params["password"] == user_params["password_confirmation"]
       @user = User.new(user_params)
       if @user.save
-        render json: @user, status: 201
+        render json: {"message": "Successfully created #{@user.name}."}, status: 201
       elsif User.find_by(email: user_params["email"])
         render json: { error: "User already exists" },
                        status: 409
